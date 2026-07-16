47df5e6ed595ab10c2dedf75e16d14942d3b8616
Oscar Garrido <oscar.garrido.o@ug.uchile.cl>
2026-07-16 02:44:52 -0400

    add cimg dependency

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,28 @@
-#include <iostream>
+#include <CImg.h>
 
-int main() {
-  std::cout << "Hello world";
+#include <argparse/argparse.hpp>
+#include <filesystem>
+#include <iostream>
+#include <string>
+
+using namespace cimg_library;
+namespace fs = std::filesystem;
+
+int main(int argc, char *argv[]) {
+  argparse::ArgumentParser parser("ascii",
+                                  "OpenCl image to ascii transformation");
+
+  parser.add_argument("image-path").help("Path to load image").required();
+  parser.add_argument("-r", "--runner")
+      .help("Runner to process image")
+      .default_value(std::string("OpenCL"));
+
+  try {
+    parser.parse_args(argc, argv);
+  } catch (const std::exception &err) {
+    std::cerr << err.what() << "\n";
+    return 1;
+  }
+
+  fs::path image_path(parser.get<std::string>("image-name"));
 }
